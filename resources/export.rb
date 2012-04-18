19411f4c112732b2dab1a5d5b6348a47d6b647f5
Michael Ivey <michael.ivey@riotgames.com>
2012-04-18 08:35:41 -0700

    Work with a copy of the current resource, and don't depend on other providers

--- resources/export.rb
+++ resources/export.rb
@@ -10,3 +10,5 @@ attribute :network, :required
 attribute :writeable, :default => false, :kind_of => [TrueClass, FalseClass]
 attribute :sync, :default => true, :kind_of => [TrueClass, FalseClass]
 attribute :extra_options, :default => ['root_squash'], :kind_of => Array
+
+attribute :exists, :kind_of => [TrueClass, FalseClass]
